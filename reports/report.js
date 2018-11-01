a926b61eb3c37a144e62e4556c54d03f5d95f01d
syrdevs <syrdevs@gmail.com>
2018-11-01 13:04:22 +0600

    report sum fixed

--- reports/report.js
+++ reports/report.js
@@ -8,13 +8,22 @@ angular.module('myApp.report', ['ngRoute'])
     .controller('ReportCtrl', ['$scope', '$rootScope', '$http', '$location', '$routeParams',
         function ($scope, $rootScope, $http, $location, $routeParams) {
 
+            $scope.reportData = [];
             $scope.typeReport = $routeParams.type;
+            $scope.sumItems = 0;
 
             if (["1", "2"].indexOf($scope.typeReport) === -1) {
                 $location.path("error/404");
+                return;
             }
 
-
-
+            $http.get('auth/report' + $scope.typeReport)
+                .then(function (res) {
+                    console.log(res);
+                    $scope.reportData = res.data;
+                    $scope.sumItems = res.data.reduce(function (a, b) {
+                        return a + b.count;
+                    }, 0);
+                })
 
         }]);
